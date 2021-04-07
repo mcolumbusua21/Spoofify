88b9792fb541058159b2b0a530c4ac0407c7b14b
Kyle Grabski <kyle.grabski@gmail.com>
2021-04-07 11:07:39 -0700

    secure client id and secret

--- config/connection.js
+++ config/connection.js
@@ -19,8 +19,8 @@ const SpotifyStrategy = require('passport-spotify').Strategy;
 passport.use(
     new SpotifyStrategy(
         {
-            clientID:  a5db0c08702e4ff4a09336b64996505f,
-            clientSecret:   b45f965b45934b2d9ed8295ace736448,
+            clientID: client_id,  
+            clientSecret: client_secret, 
             callbackURL: 'http://localhost:3001/auth/spotify/callback'
 
 
