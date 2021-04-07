e482f3e13d669322e78fd6b5842618dd8a3c0cc7
Mark Columbus <mcolumbusua21@gmail.com>
2021-04-07 11:43:02 -0700

    spotify

--- server.js
+++ server.js
@@ -1,45 +1,67 @@
-const path = require('path');
-const express = require('express');
-const session = require('express-session');
-const exphbs = require('express-handlebars');
-const routes = require('./controllers');
+require("dotenv").config();
+const passport = require("passport");
+const SpotifyStrategy = require("passport-spotify").Strategy;
+const path = require("path");
+const express = require("express");
+// const session = require('express-session');
+// const exphbs = require('express-handlebars');
+// const routes = require('./controllers');
 
-const sequelize = require('./config/connection');
+// const sequelize = require('./config/connection');
 
-const hbs = exphbs.create({});
+// const hbs = exphbs.create({});
 
-//Creating new sequelize store
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
+// //Creating new sequelize store
+// const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const sess = {
-    secret: 'Rainbow Kitten Suprise',
-    cookie: {},
-    resave: false,
-    saveUninitialized: true,
-    store: new SequelizeStore({
-        db: sequelize
-    })
-};
+// const sess = {
+//     secret: 'Rainbow Kitten Suprise',
+//     cookie: {},
+//     resave: false,
+//     saveUninitialized: true,
+//     store: new SequelizeStore({
+//         db: sequelize
+//     })
+// };
 
-//Middleware
-app.use(session(sess))
+// //Middleware
+// app.use(session(sess))
 
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
+// app.engine('handlebars', hbs.engine);
+// app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(_dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use(routes);
+// app.use(routes);
 
-sequelize.sync({ force: false }).then(() => {
-    app.listen(PORT, () => console.log('Now listening', PORT));
-  });
+// sequelize.sync({ force: false }).then(() => {
+//
+//   });
 
+passport.use(
+  new SpotifyStrategy(
+    {
+      clientID: process.env.client_id,
+      clientSecret: process.env.client_secret,
+      callbackURL: "http://localhost:3001/auth/spotify/callback",
+    },
+    function (accessToken, refreshToken, expires_in, profile, done) {
+      userInfo.findOrCreate({ spotifyId: profile.id }),
+        function (err, user) {
+          return done(err, user);
+        };
+    }
+  )
+);
+
+app.get('/', (req, res) => {
+  res.redirect('/auth/spotify')
+})
 
 app.get("/auth/spotify", passport.authenticate("spotify"), function (req, res) {
   // The request will be redirected to spotify for authentication, so this
@@ -65,3 +87,4 @@ app.get(
     // function will not be called.
   }
 );
+app.listen(PORT, () => console.log("Now listening", PORT));
