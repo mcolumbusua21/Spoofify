b44e1d1fe107f620e76298d794eaba9d670b16b6
Mark Columbus <mcolumbusua21@gmail.com>
2021-04-07 11:09:33 -0700

    server.js

--- server.js
+++ server.js
@@ -1,4 +1,44 @@
+const path = require('path');
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const routes = require('./controllers');
 
+const sequelize = require('./config/connection');
+
+const hbs = exphbs.create({});
+
+//Creating new sequelize store
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const sess = {
+    secret: 'Rainbow Kitten Suprise',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+
+//Middleware
+app.use(session(sess))
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(_dirname, 'public')));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now listening', PORT));
+  });
 
 
 app.get("/auth/spotify", passport.authenticate("spotify"), function (req, res) {
