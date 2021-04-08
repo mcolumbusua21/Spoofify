5167047d73c04a2113ee9fed6d19fcd143273029
Kyle Grabski <kyle.grabski@gmail.com>
2021-04-08 11:10:45 -0700

    working on home routes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,48 +1,52 @@
-const router = require('express').Router();
-const { User } = require('../models');
-const withAuth = require('../utils/auth')
+const router = require("express").Router();
+const { User } = require("../models");
+const withAuth = require("../utils/auth");
 const passport = require("passport");
 
 
+router.get("/", (req, res) => {
+  res.render("login");
+});
 
-router.get('/login', isAuth, (req, res) => {
-    res.redirect('/')
-  })
-  
-  router.get('/', (req, res) => {
+router.get("/login", isAuth, (req, res) => {
+  res.redirect("/");
+});
+// router.get("/auth/spotify", passport.authenticate("spotify"), function (req, res) {
+//   // The request will be redirected to spotify for authentication, so this
+//   // function will not be called.
+// });
 
-    res.render('login')
-  })
-  
-  router.get("/auth/spotify", passport.authenticate("spotify"), function (req, res) {
+router.get(
+  "/auth/spotify/callback",
+  passport.authenticate("spotify", { failureRedirect: "/login" }),
+  async function (req, res) {
+    // Successful authentication, redirect home.
+
+     res.render("login", { user: JSON.stringify(req.user) });
+
+  }
+);
+
+router.get(
+  "/auth/spotify",
+  passport.authenticate("spotify", {
+    scope: ["user-read-email", "user-read-private"],
+  }),
+  function (req, res) {
     // The request will be redirected to spotify for authentication, so this
     // function will not be called.
-  });
-  
-  router.get(
-    "/auth/spotify/callback",
-    passport.authenticate("spotify", { failureRedirect: "/login" }),
-    function (req, res) {
-      // Successful authentication, redirect home.
-      res.render('login', req.user)
-      res.redirect('/')
-    }
-  );
-  
-  router.get(
-    "/auth/spotify",
-    passport.authenticate("spotify", {
-      scope: ["user-read-email", "user-read-private"],
-    }),
-    function (req, res) {
-      // The request will be redirected to spotify for authentication, so this
-      // function will not be called.
-    }
-  );
-  
-  function isAuth(req, res, next) {
-    if (req.isAuthenticated()) return next()
-    res.redirect('/auth/spotify')
   }
+);
 
-  module.exports = router;
+router.get('/logout', (req, res) => {
+  req.session = null;
+  req.logout(); 
+  res.redirect('/');
+})
+
+function isAuth(req, res, next) {
+  if (req.isAuthenticated()) return next();
+  res.redirect("/auth/spotify");
+}
+
+module.exports = router;
---
+++ public/js/login.js
@@ -0,0 +1,6 @@
+const $loginBtn = document.querySelector("#login");
+
+$loginBtn.addEventListener("click", (e) => {
+    console.log("CLICK")
+    document.location.replace("/auth/spotify");
+  });
