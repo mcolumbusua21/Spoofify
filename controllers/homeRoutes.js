13881087c8468b19057cb8d60aa46b307bb0410f
Kyle Grabski <kyle.grabski@gmail.com>
2021-04-08 23:47:56 -0700

    working on artist api route

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,31 +1,55 @@
 const router = require("express").Router();
 const { User } = require("../models");
-const withAuth = require("../utils/auth");
 const passport = require("passport");
+var SpotifyWebApi = require('spotify-web-api-node');
 
 
-router.get("/", (req, res) => {
+let userData = [];
+let accessToken = [];
+
+router.get("/", async (req, res) => {
+  // @ ToDo NEED TO CHECK ROUTES! IF NOT LOGGED IN, GO TO LOGIN PAGE!
+  // OR ELSE MACHINE WONT RECOGNIZE NEW SPOTIFYID VARIABLE
   res.render("login");
+  
+ 
 });
 
-router.get("/login", isAuth, (req, res) => {
-  res.redirect("/");
+router.get("/login", (req, res) => {
+  // res.redirect("/"); let accessToken = userData[0].dataValues.accessToken
+  console.log("HERE IS THE TOKEN   ",accessToken)
+  // spotifyApi.setAccessToken(accessToken)
+  
+  let spotifyId = userData[0].dataValues.spotifyId
+
+  res.render("home", {user: spotifyId});
+  
 });
-// router.get("/auth/spotify", passport.authenticate("spotify"), function (req, res) {
-//   // The request will be redirected to spotify for authentication, so this
-//   // function will not be called.
-// });
 
-router.get(
-  "/auth/spotify/callback",
-  passport.authenticate("spotify", { failureRedirect: "/login" }),
-  async function (req, res) {
-    // Successful authentication, redirect home.
-
-     res.render("login", { user: JSON.stringify(req.user) });
-
+// TESTING GET ARTIST ROUTE
+router.get("/artist", async (req, res) => {
+  console.log("Artist route")
+  try{
+    let spotifyId = await userData[0].dataValues.spotifyId
+    var spotifyApi = await new SpotifyWebApi({
+      clientId:process.env.client_id ,
+      clientSecret: process.env.client_secret,
+      redirectUri: "http://localhost:3001/auth/spotify/callback"
+    });
+      spotifyApi.setAccessToken(accessToken)
+      spotifyApi.getArtistAlbums('43ZHCT0cAZBISjO8DG9PnE').then(
+        function(data) {
+          console.log('Artist albums', data.body);
+          res.render('home',{user: spotifyId, artist: data.body})
+        },
+        function(err) {
+          console.error(err);
+        }
+      );
+  }catch(err){
+    console.log("ERROR AT ARTIST ROUTE", err)
   }
-);
+})
 
 router.get(
   "/auth/spotify",
@@ -35,18 +59,42 @@ router.get(
   function (req, res) {
     // The request will be redirected to spotify for authentication, so this
     // function will not be called.
+    // res.render("login", { user: JSON.stringify(req.user) });
   }
 );
 
+router.get(
+  "/auth/spotify/callback",
+  passport.authenticate("spotify", { failureRedirect: "/login" }),
+  async function (req, res) {
+    userData = [];
+    accessToken = [];
+    await accessToken.push(req.user)
+    await userData.push(req.user)
+    // console.log("userData Array",userData)
+    // Successful authentication, redirect home.
+    // console.log(req.user)
+
+    //Redirecting to /login forces feedback loop
+     res.redirect('/login');
+
+  }
+);
+
+
+
 router.get('/logout', (req, res) => {
+  userData = [];
+  accessToken = [];
   req.session = null;
   req.logout(); 
-  res.redirect('/');
+  res.redirect('/login');
 })
 
 function isAuth(req, res, next) {
   if (req.isAuthenticated()) return next();
   res.redirect("/auth/spotify");
 }
+
 
 module.exports = router;
