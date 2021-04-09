55e9b385ad3e6d767709fd0de4f9259172f77b83
Kyle Grabski <kyle.grabski@gmail.com>
2021-04-08 23:47:47 -0700

    moved artist troubleshoot to homeroutes

--- server.js
+++ server.js
@@ -3,49 +3,53 @@ const passport = require("passport");
 const SpotifyStrategy = require("passport-spotify").Strategy;
 const path = require("path");
 const express = require("express");
-const session = require('express-session');
-const exphbs = require('express-handlebars');
-const routes = require('./controllers');
+const session = require("express-session");
+const exphbs = require("express-handlebars");
+const routes = require("./controllers");
 const { User } = require("./models");
+var SpotifyWebApi = require('spotify-web-api-node');
 
-const sequelize = require('./config/connection');
+const sequelize = require("./config/connection");
 
 const hbs = exphbs.create({});
 
 // //Creating new sequelize store
-// const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 const sess = {
-    secret: 'Rainbow Kitten Suprise',
-    resave: true,
-    saveUninitialized: true,
-    // store: new SequelizeStore({
-    //     db: sequelize
-    // })
+  secret: "Rainbow Kitten Suprise",
+  cookie: {},
+  resave: true,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+      db: sequelize
+  })
 };
 
- //Middleware
-// app.use(session(sess))
+//Middleware
+app.use(session(sess))
 
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(passport.initialize());
+// app.use(passport.session());
 app.use(routes);
 
-
 passport.serializeUser(function (user, done) {
-  done(null, user);
+  done(null, user.id);
 });
 
-passport.deserializeUser(function (obj, done) {
-  done(null, obj);
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
 });
 
 passport.use(
@@ -56,28 +60,37 @@ passport.use(
       callbackURL: "http://localhost:3001/auth/spotify/callback",
     },
     async function (accessToken, refreshToken, expires_in, profile, done) {
-      
-        // To keep the example simple, the user's spotify profile is returned to
-        // represent the logged-in user. In a typical application, you would want
-        // to associate the spotify account with a user record in your database,
-        // and return that user instead.
-        
-       const existingUser = await User.findOne({ where: {spotifyId: profile.id }});
+      // To keep the example simple, the user's spotify profile is returned to
+      // represent the logged-in user. In a typical application, you would want
+      // to associate the spotify account with a user record in your database,
+      // and return that user instead.
 
-        if(!existingUser){
-          const newUser = await User.create({
-            spotifyId: profile.id
-          })
-            return newUser;
-        }
-        return existingUser;
-      
+      const existingUser = await User.findOne({
+        where: { spotifyId: profile.id },
+      });
+
+      if (!existingUser) {
+        const newUser = await User.create({
+          spotifyId: profile.id,
+          accessToken: accessToken
+        });
+        return done(null, newUser);
+      }
+      return done(null, existingUser);
     }
   )
 );
 
 
+
+
+
+
+
+
+
+
+
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log("Now listening", PORT));
-
 });
